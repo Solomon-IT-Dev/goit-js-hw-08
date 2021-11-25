83d27b46b85272020e6bfcf9c14ce3a84f1c5a39
Roman Lysenko <rom.lysenko.23@gmail.com>
2021-11-26 00:48:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,22 +7,30 @@ const refs = {
 };
 
 const STORAGE_KEY = 'feedback-form-state';
-const userFeedbackData = {};
+const formFeedbackData = {};
+const userFormData = {
+  email: refs.formInput,
+  message: refs.formTextarea,
+};
 
 refs.feedbackForm.addEventListener('input', throttle(formInputHandler, 500));
-populateSavedData();
 refs.feedbackForm.addEventListener('submit', onSubmitFormBtnClick);
+populateSavedData();
 
 function formInputHandler(e) {
-  userFeedbackData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(userFeedbackData));
+  formFeedbackData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formFeedbackData));
 }
 
 function populateSavedData() {
   const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedFormData) {
-    refs.formInput.value = savedFormData.email;
-    refs.formTextarea.value = savedFormData.message;
+  if (savedFormData.email) {
+    userFormData.email.value = savedFormData.email;
+    formFeedbackData.email = userFormData.email.value;
+  }
+  if (savedFormData.message) {
+    userFormData.message.value = savedFormData.message;
+    formFeedbackData.message = userFormData.message.value;
   }
 }
 
@@ -31,8 +39,8 @@ function onSubmitFormBtnClick(e) {
   if (e.currentTarget.email.value === '' || e.currentTarget.message.value === '') {
     alert('Please, fill in empty areas in order to submit form!');
   } else {
-    const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    console.log('formData: ', savedFormData);
+    // const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log('formData: ', formFeedbackData);
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
   }
